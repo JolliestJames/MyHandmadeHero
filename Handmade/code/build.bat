62c408fec4d321409b0f422bff2054918d6f4baa
ToBeAmped <martinja@oregonstate.edu>
2017-01-07 22:37:32 -0800

    V1.1

--- Handmade/code/build.bat
+++ Handmade/code/build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\..\build
 pushd ..\..\build
-cl -FC -Zi ..\Handmade\code\win32_handmade.cpp user32.lib gdi32.lib
+cl -FC -Zi ..\Handmade\code\my_win32_handmade.cpp user32.lib gdi32.lib
 popd
