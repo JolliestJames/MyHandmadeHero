86265eb0caaf917fbe8533a81e57154285d6f5dd
ToBeAmped <martinja@oregonstate.edu>
2017-10-11 10:40:06 -0700

    Added .gitignore

--- Handmade/misc/shell.bat
+++ Handmade/misc/shell.bat
@@ -1,3 +1,4 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set path=w:\MyHandmadeHero\Handmade\misc;%path%
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
+REM set path=C:\Program Files (x86)\Java\jdk1.8.0_131\bin;
