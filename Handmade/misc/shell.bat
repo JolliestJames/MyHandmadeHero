6f36e65627f727d06a2ece888de4b542fdd0ddfb
ToBeAmped <martinja@oregonstate.edu>
2017-10-11 22:22:43 -0700

    Nothing to see here

--- Handmade/misc/shell.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set path=w:\MyHandmadeHero\Handmade\misc;%path%
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-REM set path=C:\Program Files (x86)\Java\jdk1.8.0_131\bin;
